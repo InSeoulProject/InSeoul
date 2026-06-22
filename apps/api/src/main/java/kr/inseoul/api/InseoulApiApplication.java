2c573fb7274dfe12ed5ccbea1bc08c5b9cfcb730
dh4m <dh4m28@gmail.com>
2026-06-22 17:39:10 +0900

    refactor(api): StubControllers 제거 및 JwtProperties 활성화

--- apps/api/src/main/java/kr/inseoul/api/InseoulApiApplication.java
+++ apps/api/src/main/java/kr/inseoul/api/InseoulApiApplication.java
@@ -1,9 +1,12 @@
 package kr.inseoul.api;
 
+import kr.inseoul.api.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class InseoulApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(InseoulApiApplication.class, args);
--- apps/api/src/test/java/kr/inseoul/api/HealthControllerTest.java
+++ apps/api/src/test/java/kr/inseoul/api/HealthControllerTest.java
@@ -4,14 +4,21 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import kr.inseoul.api.security.JwtTokenProvider;
 import kr.inseoul.api.web.HealthController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(HealthController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class HealthControllerTest {
+
+    @MockBean
+    JwtTokenProvider jwtTokenProvider;
 
     @Autowired
     MockMvc mockMvc;
